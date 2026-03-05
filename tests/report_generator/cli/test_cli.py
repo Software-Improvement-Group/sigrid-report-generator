c0536f444bdd29bff272d95de6df9836defae6cb
florisvanleeuwen <floris.vanleeuwen@softwareimprovementgroup.com>
2026-03-05 17:35:06 +0100

    Move and name integration test

--- tests/report_generator/cli/test_cli.py
+++ tests/report_generator/cli/test_cli.py
@@ -16,59 +16,8 @@ from unittest.mock import patch, MagicMock
 
 import pytest
 from click.testing import CliRunner
-from importlib_resources import files
-from tests.report_generator.cli.list_diffs import compare_pptx
 
 from report_generator.cli import run as run_cli
-
-
-@pytest.fixture
-def output_file():
-    return files("tests.report_generator.cli").joinpath("test_output.pptx")
-
-
-@pytest.fixture
-def template():
-    return files("tests.report_generator.cli").joinpath("test-template.pptx")
-
-
-@pytest.fixture
-def reference_file():
-    return files("tests.report_generator.cli").joinpath("reference_output.pptx")
-
-
-@pytest.fixture
-def customer_name():
-    return "opendemo"
-
-
-@pytest.fixture
-def system_name():
-    return "twitter-algorithm"
-
-
-@pytest.fixture
-def token():
-    return os.environ.get('REPORT_GENERATOR_TESTS_TOKEN')
-
-
-def test_generate_report(output_file, template, customer_name, system_name, token, reference_file):
-    os.environ['SIGRID_REPORT_GENERATOR_RECORD_USAGE'] = '0'
-    runner = CliRunner()
-    result = runner.invoke(run_cli, [
-        '--customer', customer_name,
-        '--system', system_name,
-        '--token', token,
-        '--template', template,
-        '--out-file', output_file,
-        '--debug'
-    ])
-
-    assert result.exit_code == 0, f"CLI command did not run successfully: {result.output}"
-    assert os.path.isfile(output_file), f"Output file {output_file} does not exist"
-
-    are_equal, differences = compare_pptx(output_file, reference_file)
-    assert are_equal, "Output file content is incorrect:" + '\n' + '\n'.join(differences)
 
 
 class TestCLIParameters:
