b78b2d56e4f51738df365ab7a4409bd67bc7385a
maximjanssen <55410274+maximjanssen@users.noreply.github.com>
2026-03-27 15:43:06 +0100

    Remove unused imports

--- src/report_generator/generator/placeholders/implementations/charts/osh_charts.py
+++ src/report_generator/generator/placeholders/implementations/charts/osh_charts.py
@@ -12,7 +12,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import logging
 from typing import Callable
 
 from pptx.chart.data import ChartData
