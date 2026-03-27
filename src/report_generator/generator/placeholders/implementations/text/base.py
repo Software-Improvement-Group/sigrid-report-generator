ed583218d2188f8b0f5a362b8c35621ef4d5ca2a
maximjanssen <55410274+maximjanssen@users.noreply.github.com>
2026-03-27 16:00:42 +0100

    Remove unused import

--- src/report_generator/generator/placeholders/implementations/text/base.py
+++ src/report_generator/generator/placeholders/implementations/text/base.py
@@ -12,7 +12,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import logging
 from abc import ABC
 from typing import Callable, Optional, Union
 
